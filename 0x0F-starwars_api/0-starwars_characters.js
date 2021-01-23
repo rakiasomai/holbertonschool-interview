87d12945b6fd6f0e6b01bfc40b8a860d4cfd327c
rakiasomai <somai.rakia@hotmail.fr>
2021-01-23 23:10:27 +0100

    one

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -8,12 +8,12 @@ async function checkcharacters (star) {
   const url = 'https://swapi-api.hbtn.io/api/films/' + star;
   let data = await (await request(url)).body;
   data = JSON.parse(data);
-  const characters = data.characters;
-  for (let y = 0; y < characters.length; y++) {
-    const urlCharacter = characters[y];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
+  const letters = data.letters;
+  for (let y = 0; y < letters.length; y++) {
+    const urlCharacter = letter[y];
+    let letter = await (await request(urlCharacter)).body;
+    letter = JSON.parse(letter);
+    console.log(letter.name);
   }
 }
 
